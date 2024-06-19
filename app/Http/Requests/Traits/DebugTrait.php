53e4dcdb5269e070269b21afaa2071fc9c288b47
Roman Nemish <roman.nemish@intelliboard.net>
2024-06-19 06:43:37 +0200

    add comment.

--- app/Http/Requests/Traits/DebugTrait.php
+++ app/Http/Requests/Traits/DebugTrait.php
@@ -8,6 +8,17 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Response;
 
+/**
+ *  Trait DebugTrait
+ *
+ *  This trait overrides the default behavior of Laravel's validation failure handling.
+ *  When validation fails, it will throw an HttpResponseException and return a JSON response
+ *  with the validation errors. This is particularly useful for handling AJAX requests
+ *  where JSON responses are expected.
+ *
+ *  Usage:
+ *  Include this trait in your FormRequest class to enable detailed JSON validation error responses.
+ */
 trait DebugTrait
 {
     protected function failedValidation(Validator $validator)
